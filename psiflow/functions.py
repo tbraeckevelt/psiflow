e6dfc05de2c207e900ba2ebe8d9acc843e8cb24b
Tom Braeckevelt <tom.braeckevelt@ugent.be>
2025-04-14 14:03:09 +0200

    ExtendedHarmonic

--- psiflow/functions.py
+++ psiflow/functions.py
@@ -178,6 +178,69 @@ class HarmonicFunction(EnergyFunction):
 
 @typeguard.typechecked
 @dataclass
+class ExtendedHarmonicFunction(EnergyFunction):
+    positions: np.ndarray
+    cell: np.ndarray
+    extended_hessian: np.ndarray
+    energy: Optional[float] = None
+    hessian_coordinates: str = "sh"
+    _Ts: np.ndarray | None = None
+    _Tc: np.ndarray | None = None
+    _TcTsinv: np.ndarray | None = None
+
+    def __post_init__(self):
+        # defintion _Ts:  s . _Ts = hessian_coords[0]
+        if self.hessian_coordinates[0] == "s":
+            self._Ts = np.eye(3)  # s = s
+        elif self.hessian_coordinates[0] == "d":
+            self._Ts = self.cell  # s . h_0 = d
+        else:
+            raise NotImplementedError(
+                f"Coordinate transformation {self.hessian_coordinates} not implemented"
+            )
+        # defintion _Tc: _Tc . cell = hessian_coords[1]
+        if self.hessian_coordinates[1] == "h":
+            self._Tc = np.eye(3)
+        elif self.hessian_coordinates[1] == "f":
+            self._Tc = np.linalg.inv(self.cell)
+        else:
+            raise NotImplementedError(
+                f"Coordinate transformation {self.hessian_coordinates} not implemented"
+            )
+        self._TcTsinv = self._Tc @ np.linalg.inv(self._Ts)
+        
+
+    def __call__(
+        self,
+        geometry: Geometry,
+    ) -> dict[str, float | np.ndarray]:
+        """ 
+        We assume that the positions naturallly scale with the cell, leading to (ds/dh)=0 and (dr_i/dh_ji)=s_j
+        This is a normal assumptions, and this is probably also assumed when calculating stresses or applying barostats
+        """
+        # defintion Tr:  r . Tr = s
+        Tr = np.linalg.inv(geometry.cell)  # r . h**(-1) = s
+        Trs = Tr @ self._Ts
+        trans_pos = geometry.per_atom.positions @ Tr @ self._Ts
+        Tgradc = self._TcTsinv @ np.linalg.pinv(trans_pos)
+
+        diff_pos  = trans_pos - self.positions @ Tr @ self._Ts
+        diff_cell = self._Tc @ (geometry.cell - self.cell)
+        delta = np.concatenate((diff_pos.reshape(-1), diff_cell.reshape(-1) ))
+        grad_T = np.dot(self.extended_hessian, delta)
+        energy = 0.5 * np.dot(delta, grad_T)
+        if self.energy is not None:
+            energy += self.energy
+        grad_pos = grad_T[:-9].reshape(-1, 3) @ Trs + Tgradc.T @ grad_T[-9:].reshape(3, 3)  # TO DO: check if this is correct
+        grad_cell = self._Tc.T @ grad_T[-9:].reshape(3, 3)                  # TO DO: check if this is correct
+        forces = (-1.0) * grad_pos
+        volume = np.linalg.det(geometry.cell)
+        stress = (1/volume) * geometry.cell.T @ grad_cell                   # TO DO: check if this is correct
+        return {"energy": energy, "forces": forces, "stress": stress}
+
+
+@typeguard.typechecked
+@dataclass
 class MACEFunction(EnergyFunction):
     model_path: str
     ncores: int
@@ -292,6 +355,7 @@ def function_from_json(path: Union[str, Path], **kwargs) -> Function:
     functions = [
         EinsteinCrystalFunction,
         HarmonicFunction,
+        ExtendedHarmonicFunction,
         MACEFunction,
         PlumedFunction,
         None,
